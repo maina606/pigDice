099e0a6750d3313f1c84c5a672c61a497d98f548
maina606 <cmmaina@gmail.com>
2019-03-04 10:18:23 +0300

    ReadMe file

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 var player1 = player1;
 var player2 = player2;
 
-// function diceRoll() {
-//   return Math.floor(Math.random()*6) + 1;
-// }
+function dice() {
+  return Math.floor(Math.random()*6) + 1;
+}
 
 //constructor - blueprint
 function dicePlayer(player, rollScore, score, tScore, color){
@@ -16,10 +16,10 @@ function dicePlayer(player, rollScore, score, tScore, color){
 }
 
 // Instances - of the object
-var player1 = new dicePlayer("player1", score1, tScore1, light blue);
-var player2 =  new dicePlayer("player2", score2, tScore2, dark blue);
+// var player1 = new dicePlayer("player1", score1, tScore1, light blue);
+// var player2 =  new dicePlayer("player2", score2, tScore2, dark blue);
 
-
+//Prototypes
 dicePlayer.prototype.rollTheDamnThing = function() {
   if (this.rollScore === 1) {
     this.score = 0;
@@ -38,9 +38,6 @@ dicePlayer.prototype.winner = function() {
     alert("Congratulations! The winner is " + this.name + "!");
   }
 }
-
-
-
 /* ************************* Algorithm ****************************
 
 if (dicePlayer1 = true && roll = 1){
@@ -74,7 +71,7 @@ if(dicePlayer1 && hold button = true){
 */
 
 // ******************************User Interface Logic ******************************
-
+/*
 function dice() {
   return Math.floor(Math.random()*6) + 1;
 }
@@ -90,25 +87,53 @@ $(document).ready(function(){
  }
 
  printNumber(result);
- document.getElementById("p1").innerHTML= result;
+ document.getElementById("p1Scored").innerHTML= result;
 
 
  event.preventDefault();
    });
  });
-
-
-
-/*
-$(document).ready(function(event){
-  $("#diceRollPlayer1").click(function() {
-    event.preventDefault();
-    player1.rollScore = rollScore();
-    $("#rollTheDice").empty()
-    $("#totalScore").empty()
-    $("#rollTheDice").append("Dice roll: " + player1.rollScore);
-    player1.rollTheDamnThing();
-    $(totalScore).append(player1.score);
-    });
-  });
 */
+
+$(document).ready(function(event){
+  $("#diceRollPlayer1").click(function(event) {
+    event.preventDefault();
+    player1.rollScore = dice();
+    $("#p1Rolled").empty()
+    $("#p1Total").empty()
+    $("#p1Rolled").append("Dice rolled: " + player1.rollScore);
+    player1.rollTheDamnThing();
+    $("#p1Total").append("Your current score is: " + player1.score);
+  });
+  $("#diceRollPlayer2").click(function() {
+    event.preventDefault();
+    player2.rollScore = rollScore();
+    $("#p2Rolled").empty()
+    $("#p2Total").empty()
+    $("#p2Rolled").append("Dice rolled: " + player2.rollScore);
+    player2.rollTheDamnThing();
+    $("#p1Total").append("Your current score is: " + player2.score);
+  });
+  $(hold).click(function(event) {
+  event.preventDefault();
+  if ($("#diceRollPlayer1").is(':visible')) {
+    player1.holdPlease();
+    $("#p1Rolled").empty();
+    $("#p1Total").empty();
+    $("#p1Total").append("Total Score for "+player1.name+" is: " + player1.tScore);
+    $(".tScore").empty();
+    $('#diceRollPlayer2').toggle();
+    $('#diceRollPlayer1').toggle();
+    player1.winner();
+  } else {
+    player2.holdPlease();
+    $("#p2Rolled").empty();
+    $("#p1Total").empty();
+    $("#p1Total").append("Total Score for "+player2.name+" is: " + player2.tScore);
+    $(".tScore").empty();
+    $("#diceRollPlayer2").toggle();
+    $("#diceRollPlayer1").toggle();
+    player2.winner();
+  }
+});
+});
